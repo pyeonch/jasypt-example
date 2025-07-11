9f8ac69901d1f97b730eab0ba64262191f60836d
pyeonch <una_p@kakao.com>
2025-07-11 12:09:56 +0900

    Init: 초기설정

---
+++ src/main/java/com/chp/jasyptpractice/JasyptPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.chp.jasyptpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasyptPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JasyptPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chp/jasyptpractice/JasyptPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chp.jasyptpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasyptPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
